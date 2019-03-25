20f307040b9ca738754049f2126fe31976e1add6
Dartrix <darwinramos2009@hotmail.com>
2019-03-24 21:38:41 -0400

    loquetuquiera

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tarea_3.Startup))]
+namespace tarea_3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
